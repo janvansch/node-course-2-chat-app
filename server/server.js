978dbdc0b167956c3f3304d438f429a10d14909f
Johannes van Schalkwyk <jvs@nimbledesign.co.za>
2018-01-05 12:21:52 +0200

    Add Message broadcasting

--- server/server.js
+++ server/server.js
@@ -17,16 +17,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.on('newMessage', (message) => {
-    console.log('New message');
-    message.at = 123;
-    socket.emit('publishMessage', message);
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
-})
+});
 
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
