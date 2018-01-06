9aa25d7a66da822be6573eb0ddf40618fe881b96
Johannes van Schalkwyk <jvs@nimbledesign.co.za>
2018-01-06 23:32:54 +0200

    Add mustache.js for message templates

--- server/server.js
+++ server/server.js
@@ -24,13 +24,11 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    console.log('createMessage', coords);
     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
