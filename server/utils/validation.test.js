fa31aaacc508e09677a96bac20896a9dc6274e59
Johannes van Schalkwyk <jvs@nimbledesign.co.za>
2018-01-07 13:56:42 +0200

    Add Users class and updateUserList event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,20 @@
+const expect = require('expect');
+
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+  it('should reject non-string values', () => {
+    var res = isRealString(98);
+    expect(res).toBe(false);
+  });
+
+  it('should reject string with only spaces', () => {
+    var res = isRealString('    ');
+    expect(res).toBe(false);
+  });
+
+  it('should allow multi word strings', () => {
+    var res = isRealString('  Should pass  ');
+    expect(res).toBe(true);
+  });
+});
